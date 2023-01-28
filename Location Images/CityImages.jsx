479559f604ab6199edbd2d2fdd59c9cd96b1eef7
Tejas Jain <1tejasjain@gmail.com>
2023-01-29 00:37:36 +0530

    deploy

---
+++ Location Images/CityImages.jsx
@@ -0,0 +1,8 @@
+import Banglore from './Banglore.png'
+import Goa from './Goa.png'
+import Jaipur from './Jaipur.png'
+import Mumbai from './Mumbai.png'
+import Delhi from  './Delhi.png'
+ 
+const CityImages = [Banglore, Goa, Jaipur, Mumbai, Delhi]
+export default CityImages
