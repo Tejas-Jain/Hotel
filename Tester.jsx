4347852a0c67037ab971bcb8ed53b6e9890e0581
tejas-jain <1tejasjain@gmail.com>
2023-09-21 15:25:58 +0530

    Clean Repo

--- Tester.jsx
+++
@@ -1,5 +0,0 @@
-import RoomSelector from "./src/Components/RoomSelector/RoomSelector";
-
-export default function Tester() {
-  return <RoomSelector />;
-}
--- public/Location Images/CityImages.jsx
+++
@@ -1,8 +0,0 @@
-import Banglore from './Banglore.png'
-import Goa from './Goa.png'
-import Jaipur from './Jaipur.png'
-import Mumbai from './Mumbai.png'
-import Delhi from  './Delhi.png'
- 
-const CityImages = [Banglore, Goa, Jaipur, Mumbai, Delhi]
-export default CityImages
